327dd74cdc71997e0f09b20aec8be5bf29b609d7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-21 10:42:44 +0200

    dev: added code for apache echarts

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,3 +1,11 @@
 import "./style.css";
+import * as echarts from "echarts";
 
-console.log("start");
+(async () => {
+  const chartDom = document.querySelector<HTMLElement>("div.map");
+  if (chartDom === null) {
+    throw new Error("Cannot find div.map");
+  }
+  const myChart = echarts.init(chartDom);
+  console.log("myChart: ", myChart);
+})();
